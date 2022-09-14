82f4e58edb50bb32afdf0ca37701d21bdbc941ad
Sean Parkin <sdparkin@protonmail.com>
2022-09-13 22:04:17 -0500

    Changes to stoplist page

--- pages/stoplist.tsx
+++ pages/stoplist.tsx
@@ -1,14 +1,36 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { Stop } from "../types/index.d";
 import { useState } from "react";
+import NavBar from "../components/NavBar";
+import SingleStop from "../components/singlestop";
 import styles from "../styles/StopList.module.css";
 
 const StopList: NextPage = () => {
-  const [stops, setStops] = useState([]);
+  const [stops, setStops] = useState<Stop[]>([]);
   const [search, setSearch] = useState("");
   const [dataLoaded, setDataLoaded] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch("/api/Notes")
+      .then((res) => res.json())
+      .then((data) => {
+        setStops(data.data);
+        setDataLoaded(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        router.push("/");
+      });
+  }, [dataLoaded]);
+  console.log(stops);
   const filteredStops = stops.filter(
     (stop) => stop.custName.toLowerCase().indexOf(search.toLowerCase()) !== -1
   );
+
   if (!filteredStops.length && dataLoaded === true) {
     return (
       <div className={styles.container}>
@@ -25,7 +47,7 @@ const StopList: NextPage = () => {
           <p>
             There were no matches for that search. Would you like to add one?
           </p>
-          <AddLink to="/add">Add</AddLink>
+          <Link href="/add">Add</Link>
         </div>
       </div>
     );
@@ -42,8 +64,8 @@ const StopList: NextPage = () => {
           placeholder="Search"
           onChange={(e) => setSearch(e.target.value)}
         />
-        {filteredStops.map((stop) => (
-          <SingleStop key={stop._id} stop={stop} />
+        {filteredStops.map((stop: Stop) => (
+          <SingleStop stop={stop} />
         ))}
       </div>
     </div>
